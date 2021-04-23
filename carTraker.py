21efb3e404d5fd2a4abc7f628d062dc5fc18a921
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-23 15:54:35 +0200

    image en noir et blanc, création de la classification

--- carTraker.py
+++ carTraker.py
@@ -9,6 +9,12 @@ classifier_file = 'car_detector.xml'
 #creation d'une image opencv
 img = cv2.imread(img_file)
 
+#convertion de l'image en noir et blanc (nécessaire pour le "haar Cascade")
+black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#création de la classification de voitures
+car_tracker = cv2.CascadeClassifier(classifier_file)
+
 #affichage de l'image
 cv2.imshow('Car detector',img)
 
