afd63059905a885d924e691e84c7000a2a415b6e
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-23 15:42:50 +0200

    test commit with pycharm creation d'une image opencv, utilisation d'un classifieur pré entrainé, affichage de l'image

--- carTraker.py
+++ carTraker.py
@@ -1,5 +1,17 @@
 import cv2
 
+#Notre image
 img_file = 'Car Image.jpeg'
 
+#Notre Classifieur de Voiture préentrainé
+classifier_file = 'car_detector.xml'
+
+#creation d'une image opencv
+img = cv2.imread(img_file)
+
+#affichage de l'image
+cv2.imshow('Car detector',img)
+
+#ne se ferme pas automatiquement (va attendre ici dan le code qu'une key soit préssé)
+cv2.waitKey()
 print('hello world')
